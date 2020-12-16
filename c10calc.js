acfdb3391fcd4f35cf7cd5f331dd1e7cf453e4ec
Kewne <67972089+kewne7768@users.noreply.github.com>
2020-12-16 17:26:38 +0100

    Add horizontal plot option

--- c10calc.js
+++ c10calc.js
@@ -32,30 +32,70 @@ function onUpdate() {
     saveStuff();
 }
 
+function getHorizontals(fieldType, baseValues) {
+    switch (fieldType) {
+        case "shopTome":
+            return {shopTome: [0, 15]};
+        case "plat8":
+            return {plat8: [0, 1, 2, 3, 4, 5]};
+        case "targetLevel":
+            let levels = [1];
+            for (let cur = baseValues.targetLevel - 2; cur <= baseValues.targetLevel + 2; ++cur) {
+                if (cur > 1 && cur <= 80) {
+                    levels.push(cur);
+                }
+            }
+            return {targetLevel: levels};
+        default:
+            return {Cost: ["For selected"]};
+    }
+}
+
 function renderTable() {
     let c15Field = document.querySelector("[name=c15]");
     let plat8Field = document.querySelector("[name=plat8]");
     let shopTomeField = document.querySelector("[name=shopTome]");
     let researchesField = document.querySelector("[name=researches]");
     let targetLevelField = document.querySelector("[name=targetLevel]");
+    let plotHorizontalField = document.querySelector("[name=plotHorizontal]");
 
-    let c15Value = fieldToNumeric(c15Field);
-    let plat8Value = fieldToNumeric(plat8Field);
-    let shopTomeValue = fieldToNumeric(shopTomeField);
-    let researchesValue = fieldToNumeric(researchesField);
-    let targetLevelValue = fieldToNumeric(targetLevelField);
+    let baseValues = {
+        c15: fieldToNumeric(c15Field),
+        plat8: fieldToNumeric(plat8Field),
+        shopTome: fieldToNumeric(shopTomeField),
+        researches: fieldToNumeric(researchesField),
+        targetLevel: fieldToNumeric(targetLevelField),
+    };
 
-    let table = document.querySelector("#resultTable tbody");
-    while (table.firstChild) {
-        table.removeChild(table.firstChild);
+    let horizontal = getHorizontals(plotHorizontalField.value, baseValues);
+    let horizontalKey = Object.keys(horizontal)[0];
+    let horizontalValues = horizontal[horizontalKey];
+
+    let table = document.querySelector("#resultTable");
+    let thead = table.querySelector("thead tr");
+    thead.querySelectorAll(".rowScope").forEach(r => r.remove());
+    for (let i = 0; i < horizontalValues.length; ++i) {
+        let th = document.createElement("th");
+        th.classList.add("rowScope");
+        th.textContent = `${horizontalKey}: ${horizontalValues[i]}`;
+        thead.appendChild(th);
     }
 
+    let tbody = table.querySelector("tbody");
+    tbody.innerHTML = "";
+
     for (let i = 0; i < hyperchallengedMultiplier.length; ++i) {
-        let cost = challengeRequirement(targetLevelValue, i, researchesValue, shopTomeValue, c15Value, plat8Value);
-        let rowHtml = `<th scope="row">Hyperchallenged ${i}</th><td>${format(cost)}</td>`;
+        let rowHtml = `<th scope="row">Hyperchallenged ${i}</th>`;
+        for (let j = 0; j < horizontalValues.length; ++j) {
+            let changed = {};
+            changed[horizontalKey] = horizontalValues[j];
+            let call = Object.assign({}, baseValues, changed);
+            let cost = challengeRequirement(call.targetLevel, i, call.researches, call.shopTome, call.c15, call.plat8);
+            rowHtml += `<td>${format(cost)}</td>`;
+        }
         let frag = document.createElement("tr");
         frag.innerHTML = rowHtml;
-        table.appendChild(frag);
+        tbody.appendChild(frag);
     }
 }
 
@@ -64,10 +104,17 @@ function loadStuff() {
     if (jsonStuff !== null) {
         try {
             let json = JSON.parse(jsonStuff);
-            let inputs = document.querySelectorAll("input");
-            inputs.forEach((tag) => {
+            document.querySelectorAll("input").forEach((tag) => {
                 if (tag.name in json) {
                     tag.value = json[tag.name];
+                }
+            });
+            document.querySelectorAll("select").forEach((tag) => {
+                if (tag.name in json) {
+                    let opt = tag.querySelector(`[value=${json[tag.name]}]`);
+                    if (opt) {
+                        tag.selectedIndex = opt.index;
+                    }
                 }
             });
         } catch (e) {
@@ -78,7 +125,7 @@ function loadStuff() {
 
 function saveStuff() {
     let obj = {};
-    let inputs = document.querySelectorAll("input");
+    let inputs = document.querySelectorAll("input,select");
     inputs.forEach((tag) => {
         if (tag.value) {
             obj[tag.name] = tag.value;
